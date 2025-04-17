9a2ce36f505bd12c0e51f0968b3990e476065026
Santiago Prado <santiagoprado2001@gmail.com>
2025-04-17 18:39:19 -0400

    bbdd lista

--- client/src/db.js
+++ client/src/db.js
@@ -9,15 +9,13 @@ export async function initDB() {
   return openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
       if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME, {
-          keyPath: 'id',
-          autoIncrement: true
-        });
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
       }
     }
   });
 }
 
+// Guardar imagen en la base de datos
 export async function saveImage(imageData) {
   const db = await initDB();
   const tx = db.transaction(STORE_NAME, 'readwrite');
@@ -27,4 +25,21 @@ export async function saveImage(imageData) {
   });
   await tx.done;
   console.log('✅ Imagen guardada en IndexedDB');
+}
+
+// Obtener todas las imágenes guardadas
+export async function getImages() {
+  console.log("Obteniendo imágenes...");
+  const db = await initDB();
+  const images = await db.getAll(STORE_NAME);
+  return images.map(item => item.image);  // Asegúrate de que solo devuelvas el `image`
+}
+
+// Borrar todas las imágenes en la base de datos
+export async function clearImages() {
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, 'readwrite');
+  await tx.store.clear();  // Borra todos los registros en el store
+  await tx.done;
+  console.log('✅ Base de datos vacía');
 }
