ba3b77254e4944d88eeb43e9f1c5f0b13b552943
Santiago Prado <santiagoprado2001@gmail.com>
2025-04-20 22:19:26 -0400

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from 'vite-plugin-wasm'
+import { VitePWA } from 'vite-plugin-gh-pages'; // Importa el plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(), VitePWA()],
+  base: '/2025-1-s1-g6-t2/', // Cambia esto a la ruta de tu proyecto
   server: {
     open: true,
   }
